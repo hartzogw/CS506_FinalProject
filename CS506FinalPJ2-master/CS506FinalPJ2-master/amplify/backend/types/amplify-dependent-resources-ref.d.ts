922f2f83332a1de71e62ec7bfb15917954eaa80c
Wiley Hartzog <62406111+hartzogw@users.noreply.github.com>
2024-05-04 14:45:33 -0500

    Add files via upload

---
+++ CS506FinalPJ2-master/CS506FinalPJ2-master/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api074336c8": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "cs506pj245965bc0": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "customerhandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
